a87a175132e86dafca3c6d72dd1459f8e6eaab3a
khostaa <sunilpoudel668@gmail.com>
2023-07-23 10:31:05 +0545

    Completed Password Generator

--- Projects/JavaScript Projects/Password Generator/index.js
+++ Projects/JavaScript Projects/Password Generator/index.js
@@ -1,18 +1,60 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const charactersSym = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
 
+let isSymbol = false
 
 function generatePassword(){
-    let gpass = document.getElementById("pass1")
+    
+    let gpass1 = document.querySelector("#pass1")
+    let gpass2 = document.querySelector("#pass2")
+    gpass1.textContent = ''
+    gpass2.textContent = ''
     for( let i=0; i<15;i++)
     {
-        gpass.textContent += characters[Math.floor((Math.random * characters.length))]
+        let num1 = Math.floor(Math.random() * characters.length)
+        gpass1.textContent += characters[num1]
+
+        
+        let num2 = Math.floor(Math.random() * characters.length)
+        gpass2.textContent += characters[num2]
     }
-    console.log(gpass)
+}
+
+function generatePasswordsym(){
+    let gpass1 = document.querySelector("#pass1")
+    let gpass2 = document.querySelector("#pass2")
+    gpass1.textContent = ''
+    gpass2.textContent = ''
+    for( let i=0; i<15;i++)
+    {
+        let num1 = Math.floor(Math.random() * charactersSym.length)
+        gpass1.textContent += charactersSym[num1]
+
+        
+        let num2 = Math.floor(Math.random() * charactersSym.length )
+        gpass2.textContent += charactersSym[num2]
+    }
+}
+function symbol(){
+    isSymbol = !isSymbol;
+    
 }
 
 
+function check()
+{
+    console.log(isSymbol)
 
+    if (isSymbol === true)
+    {
+        generatePasswordsym()
+    }
+    else  {
+        generatePassword()
+    }
+}
 
-
-
+function retheme(){
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+}
