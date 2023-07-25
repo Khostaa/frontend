5f8850debe7c8e709b9230031a7a0c184c65d05b
khostaa <sunilpoudel668@gmail.com>
2023-07-25 16:05:53 +0545

    Chrome extension added

--- Projects/JavaScript Projects/Password Generator/index.js
+++ Projects/JavaScript Projects/Password Generator/index.js
@@ -58,3 +58,18 @@ function retheme(){
     var element = document.body;
     element.classList.toggle("dark-mode");
 }
+
+function myFunction() {
+    // Get the text field
+    var copyText = document.getElementById("myInput");
+  
+    // Select the text field
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); // For mobile devices
+  
+     // Copy the text inside the text field
+    navigator.clipboard.writeText(copyText.value);
+  
+    // Alert the copied text
+    alert("Copied the text: " + copyText.value);
+  }
